7c0a9e0a7aca8599b3cc087db875807d12d8a29e
Caio Victor <caiorbcavalcante@gmail.com>
2026-04-23 16:00:52 -0300

    feat: adiciona model Usuario e AppDbContext

---
+++ UsersCrud/Models/Usuario.cs
@@ -0,0 +1,10 @@
+namespace UsersCrud.Models;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public  string Email { get; set; } = string.Empty;
+    public string Senha { get; set; } = string.Empty;
+    public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+}
